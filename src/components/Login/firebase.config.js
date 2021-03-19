2e22908ed67fbd0a949f0faf53f42ecda70752ed
Chowdhury-Anik <bloggeranik1986@gmail.com>
2021-03-19 06:11:19 +0600

    finally done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYhnruFq9sk72YQfRoTLWTjx1uVU4rBTQ",
+    authDomain: "innate-beacon-229300.firebaseapp.com",
+    projectId: "innate-beacon-229300",
+    storageBucket: "innate-beacon-229300.appspot.com",
+    messagingSenderId: "584171053777",
+    appId: "1:584171053777:web:d2079b91d7cce30d079bf0"
+};
+export default firebaseConfig;
